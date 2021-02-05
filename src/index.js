f4df2a94a671ef66eaf12ffac698fcddf4eaf920
Arthur Couto <arthurcouto010@gmail.com>
2021-02-05 13:29:06 +0000

    Adicionando uma ligação com o backend

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Api from "./accesback";
 import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Api />
   </React.StrictMode>,
   document.getElementById('root')
 );
