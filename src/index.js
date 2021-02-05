920a31677a0b50fb71c93527689b4437ab08c081
Arthur Couto <arthurcouto010@gmail.com>
2021-02-05 14:00:55 +0000

    tete

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Api from "./accesback";
 import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    
-    <Api />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
