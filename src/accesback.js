e6c0e41ae0e11532eea35e5ffc769c39ed4517f8
Arthur Couto <arthurcouto010@gmail.com>
2021-02-05 13:49:49 +0000

    update

--- src/accesback.js
+++ src/accesback.js
@@ -23,7 +23,7 @@ class Api extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
+                   
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
                 <p className="App-intro">{this.state.response}</p>
